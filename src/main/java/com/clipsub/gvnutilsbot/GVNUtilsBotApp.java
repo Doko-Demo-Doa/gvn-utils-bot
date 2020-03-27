c7df76ece471cbab8c54b3031b0851933b35033e
Quan Pham <doraemonfanclub@gmail.com>
2020-03-27 16:45:17 +0700

    Add Discord4j as fallback

--- src/main/java/com/clipsub/gvnutilsbot/GVNUtilsBotApp.java
+++ src/main/java/com/clipsub/gvnutilsbot/GVNUtilsBotApp.java
@@ -4,12 +4,17 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveAllEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 
 public class GVNUtilsBotApp extends ListenerAdapter {
+    private static int PIN_THRESHOLD = 5;
+
     public static void main(String[] args) throws LoginException, InterruptedException {
         JDA jda = JDABuilder.createDefault("")
                 .addEventListeners(new GVNUtilsBotApp())
@@ -19,5 +24,21 @@ public class GVNUtilsBotApp extends ListenerAdapter {
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         User user = event.getAuthor();
+    }
+
+    @Override
+    public void onMessageReactionAdd(@Nonnull MessageReactionAddEvent event) {
+        super.onMessageReactionAdd(event);
+
+        String pinEmoji = event.getReactionEmote().getName();
+        if (pinEmoji.equals("\uD83D\uDCCC") && event.getReaction().getCount() >= PIN_THRESHOLD) {
+            String msgId = event.getMessageId();
+            event.getChannel().pinMessageById(msgId);
+        }
+    }
+
+    @Override
+    public void onMessageReactionRemove(@Nonnull MessageReactionRemoveEvent event) {
+        // event.getChannel().getJDA().getre
     }
 }
