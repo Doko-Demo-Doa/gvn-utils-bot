8a9f9c43c2e5c1a74260955bec1e71684d59359b
Quan Pham <doraemonfanclub@gmail.com>
2020-04-09 00:35:09 +0700

    Add JavaLite

---
+++ src/main/java/app/rest/BetController.java
@@ -0,0 +1,9 @@
+package app.rest;
+
+import org.javalite.activeweb.AppController;
+import org.javalite.activeweb.annotations.RESTful;
+
+@RESTful
+public class BetController extends AppController {
+
+}
